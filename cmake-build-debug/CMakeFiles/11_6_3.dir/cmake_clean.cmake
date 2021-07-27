699b5343bccf8ee62afc907fdd4c655fb9ac2fbe
VovaDym <77827028+VovaDym@users.noreply.github.com>
2021-07-27 11:03:08 +0300

    new initial

---
+++ cmake-build-debug/CMakeFiles/11_6_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11_6_3.exe"
+  "11_6_3.exe.manifest"
+  "11_6_3.lib"
+  "11_6_3.pdb"
+  "CMakeFiles/11_6_3.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11_6_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
