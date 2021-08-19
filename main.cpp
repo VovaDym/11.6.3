b8539298b2c93aa0112ba0ea4dd1dee543c12f49
VovaDym <77827028+VovaDym@users.noreply.github.com>
2021-08-19 13:30:13 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -10,131 +10,82 @@ int n(std::string part)
     }
     return number;
 }
-
-int main() {
+bool correct (std::string part)
+{
+    bool correct;
+    for (int i = 0; i < part.length(); ++i)
+    {
+        if( part[0] == '.'|| part.length() > 3
+        || (part[0] == '0' && part[0 + 1] >= '0')
+        || (part[i] < '0' && part[i] != '.' ) || part[i] > '9')
+        {
+            correct = false;
+        }
+        else if (part[i] >= '0' && part[i] <= '9')
+        {
+            correct = true;
+        }
+    }
+    return correct;
+}
+int main()
+{
     std::string ip;
-    std::string part;
-    std::string part2;
-    std::string part3;
-    std::string part4;
     std::cout << "Enter the ip address!" << std::endl;
     std::cin >> ip ;
-    if(ip[ip.length() - 1] == '.')
+    if(ip[0] == '.' || ip[ip.length() - 1] == '.' )
     {
         std::cout << "No";
     }
     else
     {
-        bool correct = true;
-        for (int i = 0; i < ip.length(); ++i)
+        int pos = 0;
+        int pos2 = 0;
+        int pos3 = 0;
+        int dot = 0;
+        bool points = false;
+        for (int i = 0;i < ip.length();i++)
         {
-
-            if(ip[0] == '.' || (ip[0] == '0' && ip[0 + 1] >= '0')
-                || (ip[i] < '0' && ip[i] != '.' )|| ip[i] > '9')
+            if ( ip[i] == '.' && ip[i + 1] == '.')
             {
-                correct = false;
+                points = true;
                 break;
             }
-            else if (ip[i] >= '0' && ip[i] <= '9')
+            else if(dot == 0 && ip[i] == '.')
             {
-                part += ip[i];
+                pos += i;
+                ++dot;
             }
-            else if (ip[i] == '.') {
-                break;
+            else if (dot == 1 && ip[i] == '.')
+            {
+                pos2 += i;
+                ++dot;
+            }
+            else if (dot == 2 && ip[i] == '.')
+            {
+                pos3 += i;
+                ++dot;
             }
         }
-        if(!correct  || part.length() > 3 || n(part) > 255)
+        if (!points)
         {
-            std::cout << "No";
-        }
-        else
-        {
-
-            for (int i = part.length() + 1; i < ip.length(); ++i)
+            std::string part = ip.substr(0,pos);
+            std::string part2 = ip.substr(pos + 1,pos2 - (pos + 1));
+            std::string part3 = ip.substr(pos2 + 1,pos3 - (pos2 +1));
+            std::string part4 = ip.substr(pos3 + 1,ip.length() - (pos3 +1));
+            if(n(part) <= 255 &&correct(part) && n(part2) <= 255  && correct(part2)
+            && n(part3) <= 255 && correct(part3) && n(part4) <= 255 && correct(part4))
             {
-                if(ip[part.length() + 1] == '.' || (ip[i] < '0' && ip[i] != '.')
-                    || ip[i] > '9'||(ip[part.length() + 1] == '0'
-                    && ip[part.length() + 2] >= '0'))
-                {
-                    correct = false;
-                    break;
-                }
-                else if (ip[i] >= '0' && ip[i] <= '9')
-                {
-                    part2 += ip[i];
-                }
-                else if (ip[i] == '.')
-                {
-                    break;
-                }
-            }
-            if(!correct || part2.length() > 3 || n(part2) > 255)
-            {
-                std::cout << "No";
+                std::cout << "Yes";
             }
             else
             {
-
-                for (int i = part.length() + part2.length()  + 2; i < ip.length(); ++i)
-                {
-                    if(ip[part.length() + part2.length()  + 2] == '.' || (ip[i] < '0' && ip[i] != '.' )
-                        || ip[i] > '9'||(ip[part.length() + part2.length()  + 2] == '0'
-                        && ip[part.length() + part2.length()  + 3] >= '0'))
-                    {
-                        correct = false;
-                        break;
-                    }
-                    else if (ip[i] >= '0' && ip[i] <= '9')
-                    {
-                        part3 += ip[i];
-                    }
-                    else if (ip[i] == '.')
-                    {
-                        break;
-                    }
-                }
-                if (!correct || part3.length() > 3 || n(part3) > 255)
-                {
-                    std::cout << "No";
-                }
-                else
-                {
-                    for (int i = part.length() + part2.length() + part3.length() + 3; i < ip.length(); ++i)
-                    {
-                        if(ip[part.length() + part2.length() + part3.length() + 3] == '.'
-                            || (ip[i] < '0' && ip[i] != '.' )|| ip[i] > '9'
-                            ||(ip[part.length() + part2.length() + part3.length() + 3] == '0'
-                            && ip[part.length() + part2.length() + part3.length() + 4] >= '0'))
-                        {
-                            correct = false;
-                            break;
-                        }
-                        else if (ip[i] >= '0' && ip[i] <= '9')
-                        {
-                            part4 += ip[i];
-                        }
-                        else if (ip[i] == '.')
-                        {
-                            break;
-                        }
-                    }
-                    if (!correct || part4.length() > 3 || n(part4) > 255)
-                    {
-                        std::cout << "No";
-                    }
-                    else
-                    {
-                        std::cout << "Yes";
-                    }
-                }
-
+                std::cout << "No";
             }
         }
+        else
+        {
+            std::cout << "No";
+        }
     }
-
 }
-
-
-
-
-
